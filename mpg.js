6117218dcd7964dd8bdee22d025de803d38a09e4
Anupam Terkonda <41018234+Terkonda54@users.noreply.github.com>
2022-02-07 13:22:07 -0600

    HO 3-1

---
+++ mpg.js
@@ -0,0 +1,5 @@
+var floatMiles, floatGallons, floatMPG;
+floatMiles = parseFloat(prompt("Enter miles driven"));
+floatGallons = parseFloat(prompt("Enter gallons of gas used"));
+floatMPG = parseFloat(floatMiles/floatGallons).toFixed(2);
+alert("Miles per gallon = " + floatMPG);
